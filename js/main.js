efd8e7a3b08dd2ca61b309afff5c8cfff8c594d9
LexAeterna12321 <lexaeterna12321@gmail.com>
2018-10-18 09:00:44 +0200

    bouncing

--- js/main.js
+++ js/main.js
@@ -1,8 +1,10 @@
-// VARS
+// VAR
 const VAR = {
-    speed: 3,
+    speed: 5,
+    acc: 1,
 }
 
+let ball1;
 class Ball {
     constructor(x, y, r) {
         this.x = x;
@@ -12,17 +14,38 @@ class Ball {
 
     move() {
         this.x = this.x + VAR.speed;
+        if (this.x >= window.innerWidth - this.r) {
+
+            VAR.speed = -VAR.speed - VAR.acc;
+            console.log(VAR);
+        } else if (this.x <= 0) {
+            VAR.speed = -VAR.speed + VAR.acc;
+            console.log(VAR);
+        }
+
+
     };
+
+    show() {
+        stroke(255);
+        strokeWeight(4);
+        noFill();
+        ellipse(this.x, this.y, this.r, this.r);
+    }
 }
 
 function setup() {
     createCanvas(window.innerWidth, window.innerHeight);
-    const ball = new Ball(5, 5, 50);
+    ball1 = new Ball(50, 50, 50);
+    // ball2 = new Ball(50, 150, 50);
 }
 
 
 function draw() {
     background(100);
-    ball.move();
-    stroke();
+
+    ball1.move();
+    ball1.show();
+    // ball2.move();
+    // ball2.show();
 }
